24be4054a7a4eac6339bb329a1bf3b302a34b3b1
julia.klos159@gmail.com <julia.klos159@gmail.com>
2021-11-19 10:02:53 +0100

    fe: list

---
+++ src/EmployeesList.js
@@ -0,0 +1,35 @@
+import React from "react";
+class EmployeesList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const employees = this.props.employees;
+    return (
+      <div className="container">
+        <h3 className="p-3 text-center">React - Display a list of items</h3>
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employees &&
+              employees.map((employee) => (
+                <tr key={employee.id}>
+                  <td>
+                    {employee.firstName} {employee.lastName}
+                  </td>
+                  <td>{employee.email}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+export default EmployeesList;
